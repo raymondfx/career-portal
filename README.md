d9e1d2069237cd729508fd99d911d10093238741
Raymond <raymondkipngetichfx@gmail.com>
2021-01-01 21:53:04 +0300

    schema updates

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # career-portal
+***How run the career-server***
+
+cd career-portal
+
+activate virtual env
+
+``pipenv shell``
+
+inside /career-portal run
+
+install requirements.
+
+``pipenv install -r requirements.txt``
+
+run server 
+
+``python3 manage.py runserver``
+
+open port http://127.0.0.1:8080
+
+***How to run the career portal Frontend***
+
+cd Frontend
+
+run 
+
+``npm install``
+
+``npm run start``
+
+open port http://localhost:3000
--- careerportal/schema.py
+++ careerportal/schema.py
@@ -3,8 +3,36 @@ import student.schema
 import employer.schema
 import institution.schema
 
+from graphql_auth.schema import UserQuery, MeQuery
+from graphql_auth import mutations
 
-class Query(student.schema.Query,employer.schema.Query, institution.schema.Query, graphene.ObjectType):
+
+class AuthMutation(graphene.ObjectType):
+    register = mutations.Register.Field()
+    verify_account = mutations.VerifyAccount.Field()
+    resend_activation_email = mutations.ResendActivationEmail.Field()
+    send_password_reset_email = mutations.SendPasswordResetEmail.Field()
+    password_reset = mutations.PasswordReset.Field()
+    password_set = mutations.PasswordSet.Field() # For passwordless registration
+    password_change = mutations.PasswordChange.Field()
+    update_account = mutations.UpdateAccount.Field()
+    archive_account = mutations.ArchiveAccount.Field()
+    delete_account = mutations.DeleteAccount.Field()
+    send_secondary_email_activation =  mutations.SendSecondaryEmailActivation.Field()
+    verify_secondary_email = mutations.VerifySecondaryEmail.Field()
+    swap_emails = mutations.SwapEmails.Field()
+    remove_secondary_email = mutations.RemoveSecondaryEmail.Field()
+
+    # django-graphql-jwt inheritances
+    token_auth = mutations.ObtainJSONWebToken.Field()
+    verify_token = mutations.VerifyToken.Field()
+    refresh_token = mutations.RefreshToken.Field()
+    revoke_token = mutations.RevokeToken.Field()
+
+
+
+class Query(student.schema.Query,employer.schema.Query, institution.schema.Query,UserQuery, MeQuery, graphene.ObjectType):
     pass
-
-schema = graphene.Schema(query=Query)
+class Mutation(employer.schema.Mutation, graphene.ObjectType):
+    pass
+schema = graphene.Schema(query=Query, mutation=Mutation)
